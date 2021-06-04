6d2f3f881869c1c41ae8536b4e3d721b6fc8e891
Aryan Chandrakar <aryan.chandrakar2018@vitstudent.ac.in>
2021-06-04 08:22:37 +0530

    Create addition_n_number.c

---
+++ open_mp/addition_n_number.c
@@ -0,0 +1,40 @@
+// race condition
+
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	// ARYAN CHANDRAKAR 18BCI0174
+	int s=0,i,n=2021;
+	// parallel computing
+	#pragma omp parallel for 
+		for (i=1;i<=n;i++){
+			s=s+i;
+			printf("[+] Thread %d works on index%d\n", omp_get_thread_num(), i);
+		}
+		printf("[~] sum of 2021 numbers %d \n",s);	
+	// above code gives wrong answer when run again and again due to race condition refer next code
+	return 0;
+}
+
+
+// no race condition
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	// ARYAN CHANDRAKAR 18BCI0174
+	int s=0,i,n=2021;
+	// parallel computing
+	#pragma omp parallel for 
+		for (i=1;i<=n;i++){
+		#pragma omp critical
+			s=s+i;
+			printf("[~] Thread %d works on index%d\n", omp_get_thread_num(), i);
+		}
+		printf("[+] sum of 2021 numbers %d \n",s);
+		// gives correct answer due to use of critical synchronisation	
+	return 0;
+}
